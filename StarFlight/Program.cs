9a2cfcf26d089f02a2ff7db891a9cda1c0a6eebd
Roman Kovin <kovinrv@tpu.ru>
2024-12-18 17:13:09 +0700

    я первый

--- StarFlight/Program.cs
+++ StarFlight/Program.cs
@@ -6,7 +6,7 @@ namespace StarFlight
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main() // Потапенко
+        static void Main() // Потапенко, Ковин
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.

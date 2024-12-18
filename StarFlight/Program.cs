1f87f527d4ec8c7a03853ec868b182eac9b61e4f
Roman Kovin <64726389+RomanKovin@users.noreply.github.com>
2024-12-18 16:48:02 +0700

    Update Program.cs from GitHub

--- StarFlight/Program.cs
+++ StarFlight/Program.cs
@@ -11,7 +11,7 @@ namespace StarFlight
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm()); // коммент
+            Application.Run(new MainForm()); // ГЄГ®Г¬Г¬ГҐГ­ГІ from GitHub
         }
     }
 }

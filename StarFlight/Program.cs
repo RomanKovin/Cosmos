7c83f43329ddc445de7ed37a65e82bf70cb2d907
chnoq <potapenko.nikita2006@gmail.com>
2024-12-18 17:13:22 +0700

    Комментарий

--- StarFlight/Program.cs
+++ StarFlight/Program.cs
@@ -6,7 +6,7 @@ namespace StarFlight
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main() // Потапенко
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
